428359868dbde9e190b80416c02e6c5a0a913ae2
Lamont V <lxveal1983@gmail.com>
2023-02-06 15:32:56 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo512.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Muse Filez
+        </p>
+        <button
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Connect Wallet
+        </button>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
