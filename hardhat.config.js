d28276a849796a66783e5f76b740337e4deecba6
Lamont V <lxveal1983@gmail.com>
2023-02-12 15:59:35 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,41 @@
 /** @type import('hardhat/config').HardhatUserConfig */
-require("@nomiclabs/hardhat-ethers");
-
-const accessControlConditions = [
-  {
-    contractAddress: '0xA80617371A5f511Bf4c1dDf822E6040acaa63e71',
-    standardContractType: 'ERC721',
-    chain,
-    method: 'balanceOf',
-    parameters: [
-      ':userAddress'
-    ],
-    returnValueTest: {
-      comparator: '>',
-      value: '0'
-    }
-  }
-]
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-contract-sizer");
+require("dotenv").config();
 
 module.exports = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+
+    localhost: {
+      chainId: 31337,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet (network 1) it will take the first
+account as deployer. Note though that depending on how hardhat network are 
+configured, the account 0 on one network can be different than on another
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+      {
+        version: "0.4.24",
+      },
+    ],
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
+  },
 };
