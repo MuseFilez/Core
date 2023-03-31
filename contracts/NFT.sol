87112c659c83be1936319b3066b6aff50664298a
Lamont V <lxveal1983@gmail.com>
2023-03-31 09:42:45 -0400

    Update NFT.sol

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+//Single access NFT for events, one-time access
+
 contract NFT is ERC721URIStorage {
     uint public tokenCount;
     constructor() ERC721("Muse NFT", "MUSE"){}
