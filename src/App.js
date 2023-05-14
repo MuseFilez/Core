26e7dca6d1c4c23017bd0db694bbab056d8a0f88
Lamont V <lxveal1983@gmail.com>
2023-05-14 19:46:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Muse Filez
+          MFilez
         </p>
         <button
           className="App-link"
